07374387c524dbfca10fa9edeaeb3ec3552c9d97
jaranedag <jaranedag@udd.cl>
2023-02-26 19:13:00 +0000

    tarea star wars api javier araneda

--- src/app.py
+++ src/app.py
@@ -2,13 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for,g
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,People,Planets,Fav_People,Fav_Planets
+from sqlalchemy.orm import joinedload
+
 #from models import Person
 
 app = Flask(__name__)
@@ -26,6 +28,15 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+@app.before_request
+def before_request():
+    g.my_data = {}
+
+# Ejemplo de uso de g
+@app.route("/")
+def index():
+    g.my_data["message"] = "Hola mundo"
+    return jsonify(g.my_data)
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -38,12 +49,123 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    
+    all_users = User.query.all()
+    new_users = []
+    for i in range(len(all_users)):
+        print(all_users[i].serialize())
+        new_users.append(all_users[i].serialize())
+    
+   
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(new_users), 200
+@app.route("/user/favorites", methods=["GET"])
+def get_user_favorites():
+    user_email = request.args.get('email')
+    print("user_email:", user_email)
+    if user_email:
+        fav_people = Fav_People.query.filter_by(user_fav=user_email).all()
+        fav_people_list = [fav.serialize() for fav in fav_people]
+        rel_planets = Fav_Planets.query.filter_by(user_fav=user_email).all()
+        rel_planets_list = [fav.serialize() for fav in rel_planets]
+        # en postman para que se pueda listar los favoritos de un usuario envio como url https://3000-4geeksacade-flaskresthe-6gbameuuazw.ws-us88.gitpod.io/user/favorites?email=jav20green@gmail.com con ejemplo jav20green@gmail.com
+        return jsonify({'people': fav_people_list, 'planets': rel_planets_list}), 200
+    else:
+        return jsonify({'message': 'User email not provided'}), 400
+        
+@app.route("/people", methods=["GET"])
+def get_all_people():
 
-    return jsonify(response_body), 200
+    all_people = People.query.all()
+    new_people = []
+    for person in all_people:
+        new_people.append(person.serialize())
+
+    return jsonify(new_people), 200
+
+@app.route("/planets", methods=["GET"])
+def get_all_planets():
+
+    all_planets = Planets.query.all()
+    new_planets = []
+    for person in all_planets:
+        new_planets.append(person.serialize())
+
+    return jsonify(new_planets), 200
+@app.route("/people/<int:people_id>", methods=["GET"])
+def get_one_people(people_id):
+
+     # Busca la entidad People con el id dado y carga sus datos relacionados
+    person = People.query.options(joinedload(People.fav_people)).filter_by(id=people_id).first()
+    return jsonify(person.serialize()), 200
+
+@app.route("/planets/<int:planets_id>", methods=["GET"])
+def get_one_planet(planets_id):
+
+    person = Planets.query.options(joinedload(Planets.rel_planets)).filter_by(id=planets_id).first()
+    return jsonify(person.serialize()), 200
+
+@app.route("/favorite/planet/<int:planet_id>", methods=['POST'])
+def post_fav_planet(planet_id):
+    #en postman para que se pueda agregar otro favorito de un usuario envio como url https://3000-4geeksacade-flaskresthe-6gbameuuazw.ws-us88.gitpod.io/favorite/planet/5?email=jav20green@gmail.com
+
+    user_email = request.args.get('email').strip()
+    planet = Planets.query.get(planet_id)
+    new_fav_planet = Fav_Planets(planets_name=planet.name, rel_planets=planet, user_fav=user_email)
+    db.session.add(new_fav_planet)
+    db.session.commit()
+    
+    return jsonify({
+        "mensaje": "el planeta con id "+ str(planet_id) + " ha sido agregado"
+    })
+@app.route("/favorite/people/<int:people_id>", methods=['POST'])
+def post_fav_people(people_id):
+    
+    #en postman para que se pueda agregar otro favorito de un usuario envio como url https://3000-4geeksacade-flaskresthe-6gbameuuazw.ws-us88.gitpod.io/favorite/planet/5?email=jav20green@gmail.com
+
+    user_email = request.args.get('email').strip()
+    people = People.query.get(people_id)
+    new_fav_people = Fav_People(people_name=people.name, fav_people=people, user_fav=user_email)
+    db.session.add(new_fav_people)
+    db.session.commit()
+    
+    return jsonify({
+        "mensaje": "people con id "+ str(people_id) + " ha sido agregado"
+    })
+@app.route("/favorite/planet/<int:planet_id>", methods=['DELETE'])
+def del_fav_planet(planet_id):
+    
+    user_email = request.args.get('email').strip()
+    rel_planets = Fav_Planets.query.filter_by(id=planet_id, user_fav=user_email).first()
+
+    if rel_planets:
+        db.session.delete(rel_planets)
+        db.session.commit()
+        return jsonify({
+            "mensaje": "el planeta con id "+ str(planet_id) + " ha sido eliminado"
+        })
+    else:
+        return jsonify({
+            "mensaje": "No se encontró ningún personaje con el ID especificado"
+        }) 
+@app.route("/favorite/people/<int:people_id>", methods=['DELETE'])
+def del_fav_people(people_id):
+    
+    user_email = request.args.get('email').strip()
+    fav_people = Fav_People.query.filter_by(id=people_id, user_fav=user_email).first()
+
+    print(fav_people)
+    if fav_people:
+        db.session.delete(fav_people)
+        db.session.commit()
+        return jsonify({
+            "mensaje": "el personaje con id "+ str(people_id) + " ha sido eliminado"
+        })
+    else:
+        return jsonify({
+            "mensaje": "No se encontró ningún personaje con el ID especificado"
+        })    
+#para el delete nuevamente tengo que poner la url https://3000-4geeksacade-flaskresthe-6gbameuuazw.ws-us88.gitpod.io/favorite/planet/2?email=jav20green@gmail.com por ejemplo en DELETE para eliminar, pero si funciona!
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,9 +7,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+   
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +17,54 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)        
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    fav_people = db.relationship("Fav_People", backref="people")
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+class Fav_People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_name = db.Column(db.String(120), db.ForeignKey("people.name"))
+    user_fav = db.Column(db.String(120), db.ForeignKey("user.email"))
+    fav_people = db.relationship("People")
+    rel_user = db.relationship("User")   
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "people_name": self.people_name,
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)        
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    rel_planets = db.relationship("Fav_Planets", backref="planets")
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        } 
+        
+class Fav_Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planets_name = db.Column(db.String(120), db.ForeignKey("planets.name"))
+    user_fav = db.Column(db.String(120), db.ForeignKey("user.email"))
+    rel_planets = db.relationship("Planets")
+    rel_user = db.relationship("User")   
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.planets_name,
+        } 
